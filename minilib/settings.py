29178c4aefd0b06d76a0a9d41249a966eb70235a
suavesav <savinay.ng@gmail.com>
2020-06-06 15:44:06 -0700

    setup simple frontend

--- minilib/settings.py
+++ minilib/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'oak.apps.OakConfig',
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
